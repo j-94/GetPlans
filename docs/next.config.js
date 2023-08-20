f868c73877cf831d76d125fe361c8c545fbcfe0e
j-94 <45247205+j-94@users.noreply.github.com>
2023-08-20 15:32:44 +0100

    Update next.config.js

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,9 @@
 const withNextra = require('nextra')({
- 
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra( basePath: '/docs',)
+module.exports = withNextra({
+  basePath: '/docs', // This sets the base path for your Next.js app
+  // Any other Next.js configuration options can be added here
+});
