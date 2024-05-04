592ed905c7285204fea3bd3dd530bb21da459cfb
Jobs <jobs@Jobss-Laptop.local>
2024-05-04 11:23:36 +0100

    vercel errro

--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-});
-
-module.exports = withNextra({
-  basePath: '/docs', // This sets the base path for your Next.js app
-  // Any other Next.js configuration options can be added here
-});
